096bb4c03d1ade5353011eb01ab66a82991bdbd9
Juan Diego Bedoya <jdbedoya928@gmail.com>
2022-11-03 20:35:07 -0500

    nuevos cambios

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,6 +1,98 @@
-exports.getProducts=(req, res, next) =>{
+const producto=require("../models/productos")
+const fetch =(url)=>import('node-fetch').then(({default:fetch})=>fetch(url));
+
+//Ver la lista de productos
+exports.getProducts=async(req, res, next) =>{
+    const productos= await producto.find();
     res.status(200).json({
         sucess:true,
-        message: "En esta ruta usted va a poder ver todos los productos"
+        cantidad: productos.length,
+        productos
     })
 }
+
+//Ver un producto por ID
+exports.getProductById= async (req, res, next)=>{
+    const product= await producto.findById(req.params.id)
+    if (!product){
+        return res.status(404).json({
+            success:false,
+            message: "Producto no encontrado"
+        })
+    }
+    res.status(200).json({
+        sucess:true,
+        message:"Aquí encuentra la información sobre tu producto",
+        product
+    })
+}
+
+//Update para producto
+exports.updateProduct= async (req, res,next) =>{
+    let product= await producto.findById(req.params.id) //Variable de tipo modificable
+
+    if (!product){ //verifico que el objeto no existe para finalizar el proceso
+        return res.status(404).json({
+            success:false,
+            message: "Producto no encontrado"
+        })
+    }
+    //si el objeto existe, ejecuto la actualización
+    product= await producto.findByIdAndUpdate(req.params.id, req.body,{
+        new:true, //Validp atributos nuevos o actualizados
+        runValidators:true
+    });
+    //Respuesta de exito en la actualización
+    res.status(200).json({
+        success:true,
+        message:"Producto actualizado de forma exitosa",
+        product
+    })
+}
+//Eliminar producto
+exports.deleteProduct= async (req, res,next) =>{
+    const product= await producto.findById(req.params.id) //Variable de tipo modificable
+
+    if (!product){ //verifico que el objeto no existe para finalizar el proceso
+        return res.status(404).json({ //Si el objetono existe, return termina el método
+            success:false,
+            message: "Producto no encontrado"
+        })
+    }
+
+    await product.remove(); //Eliminamos el producto
+    res.status(200).json({
+        success:true,
+        message:"Producto eliminado correctamente",
+    })
+}
+
+//Crear nuevo producto /api/productos
+exports.newProduct=async(req,res,next)=>{
+    const product = await producto.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
+
+//HABLAMOS DE FETCH
+//Ver todos los productos
+function verProductos(){
+    fetch('http://localhost:4000/api/productos')
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+
+//verProductos(); Llamamos al método creado para probar la consulta
+
+//ver by ID
+function verProductoPorID(id){
+    fetch('http://localhost:4000/api/producto/'+id)
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+//verProductoPorID('63633b8657fbf56145213750'); Probamos el método con el id del producto
---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Joyeria y accesorios Esmeralda</h1>
+      <img src="./images/esmeralda1.png" alt="logo"></img>
+    </div>
+  );
+}
+
+export default App;
